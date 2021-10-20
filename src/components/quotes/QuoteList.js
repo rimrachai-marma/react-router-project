bf4124789b6c72b4bb09c56cd3f3f886a935b1da
Rimrachai Marma <60957299+rimrachai-marma@users.noreply.github.com>
2021-10-20 01:06:07 -0400

    final project

---
+++ src/components/quotes/QuoteList.js
@@ -0,0 +1,56 @@
+import { Fragment } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((QA, QB) => {
+    if (ascending) {
+      return QA.id > QB.id ? 1 : -1;
+    } else {
+      return QA.id < QB.id ? 1 : -1;
+    }
+  });
+};
+
+const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+
+  const isSortingAscending = queryParams.get("sort") === "asc";
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+  const sortinghandler = () => {
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${isSortingAscending ? "desc" : "asc"}`,
+    });
+  };
+
+  return (
+    <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={sortinghandler}>
+          Sort {isSortingAscending ? "Descending" : "Ascending"}
+        </button>
+      </div>
+
+      <ul className={classes.list}>
+        {sortedQuotes.map((quote) => (
+          <QuoteItem
+            key={quote.id}
+            id={quote.id}
+            author={quote.author}
+            text={quote.text}
+          />
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
+
+export default QuoteList;
---
+++ src/pages/AllQuotes.js
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+
+import Quotelist from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+
+const AllQuotes = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return (
+      <div className="centered">
+        <NoQuotesFound />
+      </div>
+    );
+  }
+
+  return <Quotelist quotes={loadedQuotes} />;
+};
+
+export default AllQuotes;
